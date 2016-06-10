cfd5f7e2b7a716dcc59c1a46e0a1d8ac66e94075
Hugo Sandelius <hugo.sandelius@gmail.com>
2016-06-10 15:35:18 +0200

    first version of collapsible communities

--- graphviz.js
+++ graphviz.js
@@ -1,7 +1,7 @@
 function viz(s, jsonPath) {
 
-	// convert json at jsonPath to sigma json format?
-	// OR add custom sigma parser
+    // convert json at jsonPath to sigma json format?
+    // OR add custom sigma parser
 
     sigma.parsers.json(
         jsonPath,
@@ -16,10 +16,33 @@ function viz(s, jsonPath) {
 
     function clickCollapse(s) {
         s.bind('clickNode', function(e) {
-        	// when click node in community, hide all nodes in community, show community node
-        	// when click community node, hide community node, show all nodes in community
+            // when click node in community, hide all nodes in community, show community node
+            // when click community node, hide community node, show all nodes in community
 
-            e.data.node.hidden = true;
+            clickedNode = e.data.node;
+
+            if (!clickedNode.communityNode) {
+                s.graph.nodes().forEach(function(n) {
+                    if (n.communityNode && n.label == clickedNode.community) {
+                        n.hidden = false;
+                    }
+
+                    if (!n.communityNode && n.community == clickedNode.community) {
+                        n.hidden = true;
+                    }
+                });
+            } else {
+                clickedNode.hidden = true;
+
+                s.graph.nodes().forEach(function(n) {
+                    if (!n.communityNode && n.community == clickedNode.label) {
+                        n.hidden = false;
+                    }
+                });
+            }
+
+            //e.data.node.hidden = true;
+            //console.log(e);
 
             s.refresh()
         });
