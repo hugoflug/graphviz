fae7807c7720dd6f31b4e947d07a2542aaea9d10
Hugo Sandelius <hugo.sandelius@gmail.com>
2016-06-10 15:06:10 +0200

    Added .gitignore

--- graphviz.js
+++ graphviz.js
@@ -1,4 +1,8 @@
 function viz(s, jsonPath) {
+
+	// convert json at jsonPath to sigma json format?
+	// OR add custom sigma parser
+
     sigma.parsers.json(
         jsonPath,
         s,
@@ -12,6 +16,9 @@ function viz(s, jsonPath) {
 
     function clickCollapse(s) {
         s.bind('clickNode', function(e) {
+        	// when click node in community, hide all nodes in community, show community node
+        	// when click community node, hide community node, show all nodes in community
+
             e.data.node.hidden = true;
 
             s.refresh()
