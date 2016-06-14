b4d5f9f71e3c1be665c8011a8b2c7666ddded275
Hugo Sandelius <hugo.sandelius@gmail.com>
2016-06-14 12:43:31 +0200

    added getCommunityInfo function

--- graphviz.js
+++ graphviz.js
@@ -8,30 +8,76 @@ function viz(s, jsonPath) {
         s,
         function(s) {
             s.startForceAtlas2({worker: true, barnesHutOptimize: false});
-            setTimeout(function() { s.stopForceAtlas2(); }, 10000);
+            setTimeout(function() { s.stopForceAtlas2(); }, 1000);
 
             clickCollapse(s);
         }
     );
 
+    function getCommunityInfo(s) {
+        var communities = {}
+
+        function addToCommunityInfo(source, target) {
+            var sourceNode = s.graph.nodes(source);
+            var targetNode = s.graph.nodes(target);
+
+            if (sourceNode.community !== targetNode.community) {
+                if (sourceNode.community !== undefined) {
+                    if (communities[sourceNode.community] === undefined) {
+                        var community = communities[sourceNode.community] = {};
+                    }
+
+                    if (communities[sourceNode.community][source] === undefined) {
+                        var targets = communities[sourceNode.community][source] = [];
+                    }
+
+                    targets.push({'target': target, 'community': targetNode.community})
+                }
+            }            
+        }
+
+        s.graph.edges().forEach(function(e) {
+            addToCommunityInfo(e.source, e.target);
+            addToCommunityInfo(e.target, e.source);          
+        });
+
+        return communities;
+    }
+
+    function collapse(s, communityInfo, node) {
+
+    }
+
+    function uncollapse(s, communityInfo, node) {
+        
+    }
+
     function clickCollapse(s) {
+        var info = getCommunityInfo(s)
+
+        console.log(info)
+
         s.bind('clickNode', function(e) {
-            // when click node in community, hide all nodes in community, show community node
-            // when click community node, hide community node, show all nodes in community
+            //locate intra-community nodes, color them a different color
+            //build datastructure containing all intra-community nodes
 
             clickedNode = e.data.node;
 
-            if (!clickedNode.communityNode) {
+            if (clickedNode.communityNode === false) {
                 s.graph.nodes().forEach(function(n) {
                     if (n.communityNode && n.label == clickedNode.community) {
+                        console.log(n);
+
+                        n.x = clickedNode.x;
+                        n.y = clickedNode.y;
+
                         n.hidden = false;
                     }
-
                     if (!n.communityNode && n.community == clickedNode.community) {
                         n.hidden = true;
                     }
                 });
-            } else {
+            } else if (clickedNode.communityNode === true) {
                 clickedNode.hidden = true;
 
                 s.graph.nodes().forEach(function(n) {
