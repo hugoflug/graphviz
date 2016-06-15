f8d6d27fb1eed1657e0da4f0664462294e4e9d15
Hugo Sandelius <hugo.sandelius@gmail.com>
2016-06-15 15:04:45 +0200

    added initial version of graph_layout.py

---
+++ graph_layout.py
@@ -0,0 +1,43 @@
+import sys
+import json
+import copy
+import random
+
+def graph_layout(in_json):
+    out_json = copy.deepcopy(in_json)
+
+    communities = set()
+
+    for node in out_json["nodes"]:
+        node["x"] = random.random()
+        node["y"] = random.random()
+        node["z"] = random.random()
+        node["color"] = "rgb(130,51,3)"
+        node["size"] = 1
+        node["communityNode"] = False
+        node["id"] = node["label"]
+
+        communities.append(node["community"])
+
+    for community in communities:
+        out_json["nodes"].append({
+            "x": random.random(),
+            "y": random.random(),
+            "z": random.random(),
+            "color": "rgb(130,51,3)",
+            "size": 1,
+            "communityNode": True,
+            "community": community,
+            "label": community,
+            "id": community
+        })
+
+    for edge in out_json["edges"]:
+        edge["id"] = edge["source"] + "-" + edge["target"]
+
+    return out_json
+
+if __name__ == "__main__":
+    in_json = json.load(open(sys.argv[1]))
+    out_json = graph_layout(in_json)
+    json.dump(out_json, open(out_json, 'w'))
