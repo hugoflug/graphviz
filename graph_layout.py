f0fe11c89055ed34ea939ea013d7f446a3dbc379
Hugo Sandelius <hugo.sandelius@gmail.com>
2016-07-11 15:09:00 +0200

    graph_layout now uses a list of colors instead of randomizing colors

--- graph_layout.py
+++ graph_layout.py
@@ -6,11 +6,15 @@ import networkx as nx
 from networkx.drawing.nx_agraph import graphviz_layout
 import force_atlas
 
+COLOR_LIST = ['#a6cee3', '#1f78b4', '#b2df8a', '#33a02c', '#fb9a99', '#e31a1c', '#fdbf6f', '#ff7f00', '#cab2d6', '#6a3d9a', '#ffff99']
+
 def graph_layout(in_json):
     out_json = copy.deepcopy(in_json)
 
     communities = set()
     community_colors = {}
+    color_n = 0
+    color_list = COLOR_LIST
 
     positions = calculate_positions(in_json)
 
@@ -24,7 +28,11 @@ def graph_layout(in_json):
         node["hidden"] = False
 
         if "community" in node and node["community"] not in communities:
-            community_colors[node["community"]] = "rgb(" + str(random.randint(0, 127)) + "," + str(random.randint(0, 127)) + "," + str(random.randint(0, 127)) + ")"
+            community_colors[node["community"]] = color_list[color_n]
+            color_n += 1
+            if color_n == len(color_list):
+                random.shuffle(color_list)
+                color_n = 0
             communities.add(node["community"])
 
         node["color"] = community_colors[node["community"]]
@@ -58,7 +66,7 @@ def calculate_positions(in_json):
     for edge in in_json["edges"]:
         g.add_edge(edge["source"], edge["target"])
 
-    return force_atlas.forceatlas2_layout(g, iterations=1000, nohubs=False) 
+    return force_atlas.forceatlas2_layout(g, iterations=100, nohubs=False) 
 
 if __name__ == "__main__":
     in_json = json.load(open(sys.argv[1]))
